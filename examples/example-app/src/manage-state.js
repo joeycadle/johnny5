99c23cf48624d05fdee10db2f6839e1965ef02c5
Joey Allgaier <joey@unqork.com>
2020-04-16 22:24:20 -0400

    Added simple example

--- examples/example-app/src/manage-state.js
+++ examples/example-app/src/manage-state.js
@@ -1,4 +1,30 @@
-import Johnny5 from '../../../src'
+import Johnny5, { getMachines } from 'johnny5'
+
+// Temporary until tests and proper react connect()/hook created
+const fetchUser = (id) => {
+  const users = {
+    '1': 'Joey',
+    '2': 'Daniel',
+    '3': 'Henry',
+  }
+
+  const promise = new Promise((resolve) => setTimeout(() => resolve(users[id]), 5000))
+
+  return promise
+}
+
+const getUsername = (dispatch) => async (userId) => {
+  dispatch('fetching')
+
+  try {
+    const result = await fetchUser(userId)
+    console.log('xxx', result)
+    dispatch({
+      next: 'jumping',
+      context: { user: result },
+    })
+  } catch (e) {}
+}
 
 const MachineA = Johnny5('machineA').create({
   state: 'idle',
@@ -7,23 +33,39 @@ const MachineA = Johnny5('machineA').create({
   },
   transitions: {
     idle: {
-      jumping: () => {
-        return {
-          next: 'jumping',
-          context: { foo: 'bar' },
-        }
+      getUsername,
+      consoleWarn: (dispatch) => () => {
+        console.warn('foobarbazqux')
+        dispatch('warning')
       },
     },
-    jumping: {
+    fetching: {
+      success: 'idle',
+    },
+    warning: {
       stop: 'idle',
     },
-  },
-  middlewares: {
-    onTransition: () => {},
   },
 })
 
 export function consoleLogMachine() {
-  console.log('reference', MachineA)
-  console.log('factory', Johnny5('machineA'))
+  console.log('machine from reference', MachineA)
+  console.log('machine from factory', Johnny5('machineA'))
+  console.log('machines', getMachines())
+  console.log('state', MachineA.state())
+  console.log('creator: consoleWarn', MachineA.consoleWarn())
+  console.log('state', MachineA.state())
+  console.log('creator: stop', MachineA.stop())
+  console.log('state', MachineA.state())
+  console.log('creator: getUsername', MachineA.getUsername(1))
+  console.log('state', MachineA.state())
+  console.log('context', MachineA.context())
+
+  setTimeout(() => {
+    console.log('state post timeout', MachineA.state())
+    console.log('context post timeout', MachineA.context())
+  }, 6000)
+
+  console.log('creator: success', MachineA.success())
+  console.log('state', MachineA.state())
 }
